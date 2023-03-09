b1214cfe7c679c593187d9c300fd5db15935f646
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-09 00:24:17 -0800

    bonus: added pause functionality, added more styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import MoleContainer from './MoleContainer';
 
 function App() {
   let [score, setScore] = useState(0)
+  let [start, setStart] = useState("")
+  let [pause, setPause] = useState(false)
 
   const createMoleHill = () => {
     let hills = []
@@ -23,12 +25,47 @@ function App() {
     )
   }
 
+  const handleStart = (e) => {
+    if (start === "") {
+      setStart("on")
+    } else if (start === "on") {
+      setStart("pause")
+      setPause(true)
+    } else if (start === "pause") {
+      setStart("on")
+      setPause(false)
+    }
+  }
+
+  let showGame = pause ? <div className='container' style={{ height: "612px", "align-content": "center", "justify-content": "center" }}><h1>PAUSED</h1></div> : <div>{createMoleHill()}</div>
+
+  const showScore = () => {
+    if (score == 0){
+      return (
+        <span>{score}</span>
+      )
+    } else if (score > 0) {
+      return (
+        <span style={{color: "thistle"}}>{score}</span>
+      )
+    } else {
+      return (
+        <span style={{color: "red"}}>{score}</span>
+      )
+    }
+  }
+
   return (
     <div className="App">
       <h1>REACT-A-MOLE!</h1>
-      <h2>SCORE</h2>
-      <h2>{score}</h2>
-      {createMoleHill()}
+      <h2>SCORE: {showScore()}</h2>
+      <button onClick={handleStart}>
+        {
+          pause ? <span>Resume Game</span> : <span>Pause Game</span>
+        }
+      </button>
+      {showGame}
+      {/* {createMoleHill()} */}
     </div >
   );
 }
