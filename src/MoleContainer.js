2df9297089f0f7220572ff661cccdd00e62e69a0
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-08 23:51:47 -0800

    bonus: added score decreasing functionality

--- src/MoleContainer.js
+++ src/MoleContainer.js
@@ -6,11 +6,15 @@ function MoleContainer(props) {
     let [displayMole, setDisplayMole] = useState(false)
 
     const handleClick = (e) => {
-        props.setScore(props.score + 1)
-        setDisplayMole(false)
+        if (displayMole === false) {
+            props.setScore(props.score - 1)
+        } else {
+            props.setScore(props.score + 1)
+            setDisplayMole(false)
+        }
     }
 
-    let showMole = displayMole ? <Mole setScore={props.setScore} setDisplayMole={setDisplayMole} handleClick={handleClick} /> : <EmptySlot setDisplayMole={setDisplayMole} />
+    let showMole = displayMole ? <Mole setScore={props.setScore} setDisplayMole={setDisplayMole} handleClick={handleClick} /> : <EmptySlot setScore={props.setScore} setDisplayMole={setDisplayMole} handleClick={handleClick} />
 
     return (
         <div className="mole-container">
