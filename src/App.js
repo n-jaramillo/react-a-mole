d11a818940e66b87626cf85712d16b1683dd2fa9
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-09 01:03:18 -0800

    added todos for start button and screen

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import MoleContainer from './MoleContainer';
 
 function App() {
   let [score, setScore] = useState(0)
-  let [start, setStart] = useState("")
+  // let [start, setStart] = useState(true)
   let [pause, setPause] = useState(false)
 
+  // Mole Hill
   const createMoleHill = () => {
     let hills = []
     for (let i = 0; i < 9; i++) {
@@ -25,30 +26,39 @@ function App() {
     )
   }
 
-  const handleStart = (e) => {
-    if (start === "") {
-      setStart("on")
-    } else if (start === "on") {
-      setStart("pause")
-      setPause(true)
-    } else if (start === "pause") {
-      setStart("on")
+  // TODO: Start
+  // const handleStart = (e) => {
+  //   if (start === true) {
+  //     setStart(false)
+  //   } else {
+  //     setStart(true)
+  //   }
+  // }
+
+  // Pause
+  const handlePause = (e) => {
+    if (pause === true) {
       setPause(false)
+    } else {
+      setPause(true)
     }
   }
 
+  // Pause Screen
+  let showGame = pause ? <div className='container' style={{ height: "612px", "align-content": "center", "justify-content": "center" }}><h1>PAUSED</h1></div> : <div>{createMoleHill()}</div>
+
+  // Reset
   const handleReset = (e) => {
     if (window.confirm("Press OK to restart game\n\nOR\n\nPress cancel to continue playing")) {
-      setStart("")
+      // setStart(true)
       setPause(false)
       setScore(0)
     }
   }
 
-  let showGame = pause ? <div className='container' style={{ height: "612px", "align-content": "center", "justify-content": "center" }}><h1>PAUSED</h1></div> : <div>{createMoleHill()}</div>
-
+  // Score Styling
   const showScore = () => {
-    if (score == 0) {
+    if (score === 0) {
       return (
         <span>{score}</span>
       )
@@ -66,19 +76,23 @@ function App() {
   return (
     <div className="App">
       <h1>REACT-A-MOLE!</h1>
-      <h2>SCORE: {showScore()}</h2>
+      {/* TODO: Start Button */}
+      {/* <button style={{ visibility: { start } ? "visible" : "hidden" }} onClick={handleStart}>Start Game</button> */}
+      {/* <div style={{ visibility: { start } ? "hidden" : "visible" }}> */}
       <div>
-        <button onClick={handleStart}>
-          {
-            pause ? <span>Resume Game</span> : <span>Pause Game</span>
-          }
-        </button>
-        <button className='restart' onClick={handleReset}>
-          <span>Restart Game</span>
-        </button>
+        <h2>SCORE: {showScore()}</h2>
+        <div>
+          <button onClick={handlePause}>
+            {
+              pause ? <span>Resume Game</span> : <span>Pause Game</span>
+            }
+          </button>
+          <button className='restart' onClick={handleReset}>
+            <span>Restart Game</span>
+          </button>
+        </div>
+        {showGame}
       </div>
-      {showGame}
-      {/* {createMoleHill()} */}
     </div >
   );
 }
